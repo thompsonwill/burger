d17b4e6d3e026049687d43d7cf2725d20294c3a5
Will <williamjamesthompson@gmail.com>
2018-09-05 12:16:10 -0500

    Having problems with connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Max Burger", 0);
